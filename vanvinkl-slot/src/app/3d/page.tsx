9cab71ba1877544cb755f44df290ca72defb3b88
385Bojan <bojan.petkovic@385productions.com>
2026-01-09 18:41:48 +0100

    fix: force rebuild - add comment to /3d page

--- vanvinkl-slot/src/app/3d/page.tsx
+++ vanvinkl-slot/src/app/3d/page.tsx
@@ -2,6 +2,7 @@
 
 import { CasinoLoungeUltra } from '@/components/casino'
 
+// Fixed: R3F hooks error - CasinoLoungeUltra has Canvas wrapper
 export default function Casino3DPage() {
   return <CasinoLoungeUltra />
 }
